7467925b1f74b70541dc168855314514e317f9ca
MoldsonBrown <nelson.ceballos@crowdaronline.com>
2020-10-01 18:35:45 -0300

    Ejercicio Mobile

--- src/main/java/com/crowdar/examples/services/HomeService.java
+++ src/main/java/com/crowdar/examples/services/HomeService.java
@@ -8,4 +8,40 @@ public class HomeService {
         MobileActionManager.waitVisibility("HomeView.startButton");
         Assert.assertTrue(MobileActionManager.isVisible("HomeView.startButton"));
     }
+
+    public static void doLoginProcess(String email, String password){
+        MobileActionManager.setInput("HomeView.emailInput",email);
+        MobileActionManager.setInput("HomeView.passwordInput", password);
+        MobileActionManager.click("HomeView.signInButton");
+        MobileActionManager.click("HomeView.acceptOpenCamButton");
+        MobileActionManager.click("HomeView.cameraButton");
+    }
+
+    public static void TapsOnCheckInButton() {
+        MobileActionManager.click("HomeView.checkInButton");
+        MobileActionManager.click("HomeView.cameraButton");
+        MobileActionManager.waitVisibility("HomeView.errorReportButton");
+        MobileActionManager.click("HomeView.errorReportButton");
+        MobileActionManager.click("HomeView.modalStartButton");
+    }
+
+    public static void isCheckOutButtonLoaded() {
+        MobileActionManager.waitVisibility("HomeView.checkOutButton");
+        Assert.assertTrue(MobileActionManager.isVisible("HomeView.checkOutButton"));
+
+    }
+
+    public static void TapsOnCheckOutButton() {
+        MobileActionManager.click("HomeView.checkOutButton");
+        MobileActionManager.click("HomeView.modalFinishButton");
+    }
+
+    public static void TapsOnFinishButton() {
+        MobileActionManager.click("HomeView.finishButton");
+    }
+
+    public static void isTimeButtonLoaded() {
+        MobileActionManager.waitVisibility("HomeView.timeButton");
+        Assert.assertTrue(MobileActionManager.isVisible("HomeView.timeButton"));
+    }
 }
--- src/main/java/com/crowdar/examples/steps/HomeSteps.java
+++ src/main/java/com/crowdar/examples/steps/HomeSteps.java
@@ -2,10 +2,42 @@ package com.crowdar.examples.steps;
 
 import com.crowdar.core.PageSteps;
 import com.crowdar.examples.services.HomeService;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class HomeSteps extends PageSteps {
 
+    @Given("The user does login process with: '(.*)', '(.*)'")
+    public void doLoginProcess(String email, String password) {
+           HomeService.doLoginProcess(email, password);
+    }
+
+    @When("The user taps on CheckInButton")
+    public void theUserTapsOnCheckInButton() {
+        HomeService.TapsOnCheckInButton();
+    }
+
+    @And("The button CheckOutButton is displayed")
+    public void theButtonCheckOutButtonIsDisplayed() {
+        HomeService.isCheckOutButtonLoaded();
+    }
+
+    @And("The user taps on CheckOutButton")
+    public void theUserTapsOnCheckOutButton() {
+        HomeService.TapsOnCheckOutButton();
+    }
+
+    @And("The total time spent is displayed")
+    public void theTotalTimeSpentIsDisplayed() {
+        HomeService.isTimeButtonLoaded();
+    }
+
+    @And("The user taps on FinishButton")
+    public void theUserTapsOnFinishButton() {
+        HomeService.TapsOnFinishButton();
+    }
     @Then("The Homepage is displayed")
     public void theHomepageIsDisplayed() {
         HomeService.isViewLoaded();
